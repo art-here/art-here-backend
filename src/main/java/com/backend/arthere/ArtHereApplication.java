1e464388640bef8505e405c93bb0234683c69766
ensk26 <tjensk26@gmail.com>
2023-01-17 01:02:05 +0900

    chore: 외부에 있는 real-application.yml 파일을 불러오기 위한 Art-Here-Application 설정

--- src/main/java/com/backend/arthere/ArtHereApplication.java
+++ src/main/java/com/backend/arthere/ArtHereApplication.java
@@ -1,13 +1,21 @@
 package com.backend.arthere;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // JPA Auditing 활성화
 @SpringBootApplication
 public class ArtHereApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ArtHereApplication.class, args);
-	}
+    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "classpath:application.yml,"
+            + "/app/config/art-here-backend/real-application.yml";
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(ArtHereApplication.class)
+                .properties(APPLICATION_LOCATIONS)
+                .run(args);
+    }
 
 }
