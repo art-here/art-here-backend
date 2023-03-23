fcb27b519779be2512f909654194b1f2343b76a6
ensk26 <tjensk26@gmail.com>
2023-03-23 22:56:19 +0900

    feat: 컴포넌트 스캔 대상 제외를 위한 어노테이션 추가

--- src/main/java/com/backend/arthere/global/config/AdminS3Config.java
+++ src/main/java/com/backend/arthere/global/config/AdminS3Config.java
@@ -6,11 +6,13 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.backend.arthere.global.ExcludeComponent;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ExcludeComponent
 public class AdminS3Config {
 
     @Value("${aws.s3.region}")
